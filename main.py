2254ddcba1b20341258cb178d98b9468b3d0b687
Yorai Levi <yorai.email@gmail.com>
2022-05-05 02:20:12 +0300

    May 5, 2022 2:20 AM

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
-# https://www.reddit.com/r/pushshift/comments/aibhec/reddit_comments_working_with_zst_files/
-# zstd -cdq ../../Datasets/Reddit/set1/Reddit_Subreddits.ndjson.zst
 import ujson as json
 import dask.bag as db
 
-if __name__ == '__main__':
-    bag = db.read_text("RC_2006-10.zst").map(json.loads)
-    
-    df = bag.to_dataframe().head()
+
+metaComment = [
+    ("gilded", int),
+    ("retrieved_on", int),
+    ("distinguished", str),
+    ("author_flair_text", str),
+    ("author", str),
+    ("edited", bool),
+    ("parent_id", str),
+    ("id", str),
+    ("subreddit", str),
+    ("author_flair_css_class", str),
+    ("created_utc", int),
+    ("score", int),
+    ("ups", int),
+    ("controversiality", int),
+    ("body", str),
+    ("link_id", str),
+    ("stickied", bool),
+    ("subreddit_id", str),
+]
+bag = db.read_text("RC_2006-10.zst").map(json.loads)
+df = bag.to_dataframe(meta=metaComment).head()
