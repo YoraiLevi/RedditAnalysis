b075524fcd6e50e1f0fe3ccafa61a45b01cedd3f
Yorai Levi <yorai.email@gmail.com>
2022-05-05 00:59:38 +0300

    May 5, 2022 12:59 AM

--- main.py
+++ main.py
@@ -5,6 +5,5 @@ import dask.bag as db
 
 if __name__ == '__main__':
     bag = db.read_text("RC_2020-10.zst").map(json.loads)
-    # df = bag.to_dataframe()
-    bag.count().compute()
-    exit()
+    df = bag.to_dataframe().head()
+    # bag.count().compute()
