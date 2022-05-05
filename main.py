87f8c82b8f07392849eb63f66cf021e4e15e6156
Yorai Levi <yorai.email@gmail.com>
2022-05-05 22:53:54 +0300

    May 5, 2022 10:53 PM

--- main.py
+++ main.py
@@ -1,68 +1,68 @@
 import ujson as json
 import dask.bag as db
 from zreader import Zreader
-from dask.distributed import Client, progress
+from dask.distributed import progress, Client, LocalCluster
 from dask import delayed
 
-
+metaComment = [
+    ("gilded", int),
+    ("retrieved_on", int),
+    ("distinguished", str),
+    ("author_flair_text", str),
+    ("author", str),
+    ("edited", bool),
+    ("parent_id", str),
+    ("id", str),
+    ("subreddit", str),
+    ("author_flair_css_class", str),
+    ("created_utc", int),
+    ("score", int),
+    ("ups", int),
+    ("controversiality", int),
+    ("body", str),
+    ("link_id", str),
+    ("stickied", bool),
+    ("subreddit_id", str),
+]
+filenames = [
+    # "D:/Downloads/reddit/comments/RC_2006-01.zst",
+    # "D:/Downloads/reddit/comments/RC_2006-02.zst",
+    # "D:/Downloads/reddit/comments/RC_2006-03.zst",
+    # "D:/Downloads/reddit/comments/RC_2006-04.zst",
+    # "D:/Downloads/reddit/comments/RC_2006-05.zst",
+    # "D:/Downloads/reddit/comments/RC_2006-06.zst",
+    # "D:/Downloads/reddit/comments/RC_2006-07.zst",
+    # "D:/Downloads/reddit/comments/RC_2006-08.zst",
+    # "D:/Downloads/reddit/comments/RC_2006-09.zst",
+    # "D:/Downloads/reddit/comments/RC_2006-10.zst",
+    # "D:/Downloads/reddit/comments/RC_2006-11.zst",
+    # "D:/Downloads/reddit/comments/RC_2006-12.zst",
+    # "D:/Downloads/reddit/comments/RC_2007-01.zst",
+    # "D:/Downloads/reddit/comments/RC_2007-02.zst",
+    # "D:/Downloads/reddit/comments/RC_2007-03.zst",
+    # "D:/Downloads/reddit/comments/RC_2015-05.zst",
+    # "D:/Downloads/reddit/comments/RC_2017-06.zst",
+    # "D:/Downloads/reddit/comments/RC_2018-02.zst",
+    # "D:/Downloads/reddit/comments/RC_2019-05.zst",
+    # "D:/Downloads/reddit/comments/RC_2019-11.zst",
+    # "D:/Downloads/reddit/comments/RC_2020-07.zst",
+    # "D:/Downloads/reddit/comments/RC_2020-08.zst",
+    # "D:/Downloads/reddit/comments/RC_2020-09.zst",
+    "D:/Downloads/reddit/comments/RC_2020-10.zst",
+    # "D:/Downloads/reddit/comments/RC_2020-11.zst",
+    # "D:/Downloads/reddit/comments/RC_2020-12.zst",
+    # "D:/Downloads/reddit/comments/RC_2021-01.zst",
+    # "D:/Downloads/reddit/comments/RC_2021-02.zst",
+    # "D:/Downloads/reddit/comments/RC_2021-03.zst",
+    # "D:/Downloads/reddit/comments/RC_2021-04.zst",
+    # "D:/Downloads/reddit/comments/RC_2021-05.zst",
+    # "D:/Downloads/reddit/comments/RC_2021-06.zst"
+]
 
 
 def main():
-    client = Client(threads_per_worker=1, n_workers=4)
-    metaComment = [
-        ("gilded", int),
-        ("retrieved_on", int),
-        ("distinguished", str),
-        ("author_flair_text", str),
-        ("author", str),
-        ("edited", bool),
-        ("parent_id", str),
-        ("id", str),
-        ("subreddit", str),
-        ("author_flair_css_class", str),
-        ("created_utc", int),
-        ("score", int),
-        ("ups", int),
-        ("controversiality", int),
-        ("body", str),
-        ("link_id", str),
-        ("stickied", bool),
-        ("subreddit_id", str),
-    ]
-    filenames = [
-        # "D:/Downloads/reddit/comments/RC_2006-01.zst",
-        # "D:/Downloads/reddit/comments/RC_2006-02.zst",
-        # "D:/Downloads/reddit/comments/RC_2006-03.zst",
-        # "D:/Downloads/reddit/comments/RC_2006-04.zst",
-        # "D:/Downloads/reddit/comments/RC_2006-05.zst",
-        # "D:/Downloads/reddit/comments/RC_2006-06.zst",
-        # "D:/Downloads/reddit/comments/RC_2006-07.zst",
-        # "D:/Downloads/reddit/comments/RC_2006-08.zst",
-        # "D:/Downloads/reddit/comments/RC_2006-09.zst",
-        # "D:/Downloads/reddit/comments/RC_2006-10.zst",
-        # "D:/Downloads/reddit/comments/RC_2006-11.zst",
-        # "D:/Downloads/reddit/comments/RC_2006-12.zst",
-        # "D:/Downloads/reddit/comments/RC_2007-01.zst",
-        # "D:/Downloads/reddit/comments/RC_2007-02.zst",
-        # "D:/Downloads/reddit/comments/RC_2007-03.zst",
-        # "D:/Downloads/reddit/comments/RC_2015-05.zst",
-        # "D:/Downloads/reddit/comments/RC_2017-06.zst",
-        # "D:/Downloads/reddit/comments/RC_2018-02.zst",
-        # "D:/Downloads/reddit/comments/RC_2019-05.zst",
-        # "D:/Downloads/reddit/comments/RC_2019-11.zst",
-        # "D:/Downloads/reddit/comments/RC_2020-07.zst",
-        # "D:/Downloads/reddit/comments/RC_2020-08.zst",
-        # "D:/Downloads/reddit/comments/RC_2020-09.zst",
-        "D:/Downloads/reddit/comments/RC_2020-10.zst",
-        # "D:/Downloads/reddit/comments/RC_2020-11.zst",
-        # "D:/Downloads/reddit/comments/RC_2020-12.zst",
-        # "D:/Downloads/reddit/comments/RC_2021-01.zst",
-        # "D:/Downloads/reddit/comments/RC_2021-02.zst",
-        # "D:/Downloads/reddit/comments/RC_2021-03.zst",
-        # "D:/Downloads/reddit/comments/RC_2021-04.zst",
-        # "D:/Downloads/reddit/comments/RC_2021-05.zst",
-        # "D:/Downloads/reddit/comments/RC_2021-06.zst"
-    ]
+    cluster = LocalCluster()
+    client = Client(cluster)
     load = lambda filename: Zreader(filename).readlines()
     # db.from_delayed([delayed(load)(filename) for filename in filenames])
     # bag = db.from_sequence(filenames).map().map(json.loads)
