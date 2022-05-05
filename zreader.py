caf3ca718e2af7aa5b19cf288e5692d21fd61170
Yorai Levi <yorai.email@gmail.com>
2022-05-05 18:01:11 +0300

    May 5, 2022 6:01 PM

---
+++ zreader.py
@@ -0,0 +1,16 @@
+import zstandard as zstd
+import io
+
+class Zreader:
+    def __init__(self, file):
+        """Init method"""
+        self.fh = open(file, "rb")
+        self.dctx = zstd.ZstdDecompressor(max_window_size=2147483648)
+        self.reader = self.dctx.stream_reader(self.fh)
+        self.text = io.TextIOWrapper(self.reader, encoding="utf-8")
+
+    def readlines(self):
+        """Generator method that creates an iterator for each line of JSON"""
+        # stream_reader = self.dctx.stream_reader(fh)
+        for line in self.text:
+            pass
