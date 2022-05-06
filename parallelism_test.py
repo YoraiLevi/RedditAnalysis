a0642d4b2fe8b3c0825d998a49dbca4e2869b50d
Yorai Levi <yorai.email@gmail.com>
2022-05-06 13:12:57 +0300

    May 6, 2022 1:12 PM

--- parallelism_test.py
+++ parallelism_test.py
@@ -5,13 +5,14 @@ from dask import delayed
 from dask.distributed import Client, progress
 from itertools import count
 
+N = 10**8
 def load():
-    return delayed(count)
+    return delayed(range(N))
 if __name__ == '__main__':
 
     client = Client(threads_per_worker=2, n_workers=2)
 
-    bag = db.read_text(name).map(json.loads)
+    bag = db.from_delayed([load()]).
     print(out)
 # df = bag.to_dataframe(meta=metaComment).body
 # bag.to_dataframe(meta=metaComment).body.str.normalize('NFKD').str.lower().split().compute() 
