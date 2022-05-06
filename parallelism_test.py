beaf3e6e07581b9b1a3e1e89a700dbed714c3c78
Yorai Levi <yorai.email@gmail.com>
2022-05-06 13:09:56 +0300

    May 6, 2022 1:09 PM

---
+++ parallelism_test.py
@@ -0,0 +1,12 @@
+import ujson as json
+import dask.bag as db
+from dask.distributed import Client, progress
+from itertools import count
+
+if __name__ == '__main__':
+    client = Client(threads_per_worker=2, n_workers=2)
+    bag = db.read_text(name).map(json.loads)
+    print(out)
+# df = bag.to_dataframe(meta=metaComment).body
+# bag.to_dataframe(meta=metaComment).body.str.normalize('NFKD').str.lower().split().compute() 
+# a = bag.map(lambda x:x['body']).str.lower().str.rstrip().str.lstrip().str.split().flatten().compute()
