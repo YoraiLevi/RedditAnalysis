7711682ab3fe6b50cea51395d532f3748b88a1aa
Yorai Levi <yorai.email@gmail.com>
2022-05-06 13:11:09 +0300

    May 6, 2022 1:11 PM

--- parallelism_test.py
+++ parallelism_test.py
@@ -1,10 +1,16 @@
+from turtle import delay
 import ujson as json
 import dask.bag as db
+from dask import delayed
 from dask.distributed import Client, progress
 from itertools import count
 
+def load():
+    return delayed(count)
 if __name__ == '__main__':
+
     client = Client(threads_per_worker=2, n_workers=2)
+
     bag = db.read_text(name).map(json.loads)
     print(out)
 # df = bag.to_dataframe(meta=metaComment).body
