c190e69b4690a623626a464a7e8cf50ffdfffd25
Yorai Levi <yorai.email@gmail.com>
2022-05-06 13:07:14 +0300

    May 6, 2022 1:07 PM

--- main.py
+++ main.py
@@ -1,118 +1,34 @@
-from sched import scheduler
-from numpy import iterable
 import ujson as json
 import dask.bag as db
-from zreader import Zreader
-from dask.distributed import progress, Client, LocalCluster,as_completed
-from dask import delayed
-import dask
+from dask.distributed import Client, progress
 
-metaComment = [
-    ("gilded", int),
-    ("retrieved_on", int),
-    ("distinguished", str),
-    ("author_flair_text", str),
-    ("author", str),
-    ("edited", bool),
-    ("parent_id", str),
-    ("id", str),
-    ("subreddit", str),
-    ("author_flair_css_class", str),
-    ("created_utc", int),
-    ("score", int),
-    ("ups", int),
-    ("controversiality", int),
-    ("body", str),
-    ("link_id", str),
-    ("stickied", bool),
-    ("subreddit_id", str),
-]
-filenames = [
-    # "D:/Downloads/reddit/comments/RC_2006-01.zst",
-    # "D:/Downloads/reddit/comments/RC_2006-02.zst",
-    # "D:/Downloads/reddit/comments/RC_2006-03.zst",
-    # "D:/Downloads/reddit/comments/RC_2006-04.zst",
-    # "D:/Downloads/reddit/comments/RC_2006-05.zst",
-    # "D:/Downloads/reddit/comments/RC_2006-06.zst",
-    # "D:/Downloads/reddit/comments/RC_2006-07.zst",
-    # "D:/Downloads/reddit/comments/RC_2006-08.zst",
-    # "D:/Downloads/reddit/comments/RC_2006-09.zst",
-    # "D:/Downloads/reddit/comments/RC_2006-10.zst",
-    # "D:/Downloads/reddit/comments/RC_2006-11.zst",
-    # "D:/Downloads/reddit/comments/RC_2006-12.zst",
-    # "D:/Downloads/reddit/comments/RC_2007-01.zst",
-    # "D:/Downloads/reddit/comments/RC_2007-02.zst",
-    # "D:/Downloads/reddit/comments/RC_2007-03.zst",
-    # "D:/Downloads/reddit/comments/RC_2015-05.zst",
-    # "D:/Downloads/reddit/comments/RC_2017-06.zst",
-    # "D:/Downloads/reddit/comments/RC_2018-02.zst",
-    # "D:/Downloads/reddit/comments/RC_2019-05.zst",
-    # "D:/Downloads/reddit/comments/RC_2019-11.zst",
-    # "D:/Downloads/reddit/comments/RC_2020-07.zst",
-    # "D:/Downloads/reddit/comments/RC_2020-08.zst",
-    # "D:/Downloads/reddit/comments/RC_2020-09.zst",
-    # "D:/Downloads/reddit/comments/RC_2020-10.zst",
-    # "D:/Downloads/reddit/comments/RC_2020-11.zst",
-    # "D:/Downloads/reddit/comments/RC_2020-12.zst",
-    # "D:/Downloads/reddit/comments/RC_2021-01.zst",
-    "D:/Downloads/reddit/comments/RC_2021-02.zst",
-    # "D:/Downloads/reddit/comments/RC_2021-03.zst",
-    # "D:/Downloads/reddit/comments/RC_2021-04.zst",
-    # "D:/Downloads/reddit/comments/RC_2021-05.zst",
-    # "D:/Downloads/reddit/comments/RC_2021-06.zst"
-]
-
-
-def chunk(iterable,chunk_size=10*6):
-    iterator = iter(iterable)
-    while(True):
-        buffer = [None]*chunk_size
-        try:
-            for i in range(chunk_size):
-                buffer[i] = next(iterator)
-            yield buffer
-        except StopIteration:
-            yield buffer[:i]
-            return
-# @delayed
-def load(filename,buffer_size=10*6):
-    # print('load called')
-    iterable = Zreader(filename).readlines()
-    return iterable#chunk(iterable=iterable)
-
-def main():
-    cluster = LocalCluster()
-    client = Client(cluster)
-    # db.from_delayed([delayed(load)(filename) for filename in filenames])
-    # bag = db.from_sequence(filenames).map().map(json.loads)
-    # bag = db.from_delayed([delayed(load)(filename) for filename in filenames])
-    # bag = db.from_delayed([load(filename) for filename in filenames])
-for chunk in load(filenames[0]):
-    print(1)
-    client.map(json.loads,chunk)
-
-    data = client.scatter(load(filenames[0]))  
-    res = client.submit(json.loads, data)
-    # futures = client.scatter(bag)
-    # as_completed(futures).flatten().map(json.loads)
-    # frequencyList = (
-    #     bag.map(lambda x: x["body"])
-    #     .str.lower()
-    #     .str.rstrip()
-    #     .str.lstrip()
-    #     .str.split()
-    #     .flatten()
-    #     .frequencies(sort=True)
-    # )
-
-    # out = frequencyList.to_dataframe().to_csv("2021-*.csv")
-    # print(out)
-
-
+if __name__ == '__main__':
+    client = Client(threads_per_worker=2, n_workers=2)
+    metaComment = [
+        ("gilded", int),
+        ("retrieved_on", int),
+        ("distinguished", str),
+        ("author_flair_text", str),
+        ("author", str),
+        ("edited", bool),
+        ("parent_id", str),
+        ("id", str),
+        ("subreddit", str),
+        ("author_flair_css_class", str),
+        ("created_utc", int),
+        ("score", int),
+        ("ups", int),
+        ("controversiality", int),
+        ("body", str),
+        ("link_id", str),
+        ("stickied", bool),
+        ("subreddit_id", str),
+    ]
+    name = "RC_2021-*.zst"
+    bag = db.read_text(name).map(json.loads)
+    frequencyList = bag.map(lambda x:x['body']).str.lower().str.rstrip().str.lstrip().str.split().flatten().frequencies(sort=True)
+    out = frequencyList.to_dataframe().to_csv('2021-*.csv')
+    print(out)
 # df = bag.to_dataframe(meta=metaComment).body
-# bag.to_dataframe(meta=metaComment).body.str.normalize('NFKD').str.lower().split().compute()
+# bag.to_dataframe(meta=metaComment).body.str.normalize('NFKD').str.lower().split().compute() 
 # a = bag.map(lambda x:x['body']).str.lower().str.rstrip().str.lstrip().str.split().flatten().compute()
-
-#  bag = db.from_sequence(filenames).map_partitions(lambda filename: Zreader(filename).readlines)
-if __name__ == "__main__":
-    main()
