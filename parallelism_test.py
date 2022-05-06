e232b1d07f0f04b102cd007a8b4a51971df0d09a
Yorai Levi <yorai.email@gmail.com>
2022-05-06 14:12:08 +0300

    May 6, 2022 2:12 PM

--- parallelism_test.py
+++ parallelism_test.py
@@ -3,15 +3,11 @@ from dask import delayed
 from dask.distributed import Client, LocalCluster
 
 N = 10**6
-def gen():
-    for i in range(N):
-        yield delayed(i)
-
 def load():
     return delayed(range(N))
 if __name__ == '__main__':
     client = Client(LocalCluster())
-    bag = db.from_delayed([gen(),gen()]).map(lambda x: 2*x)
+    bag = db.from_delayed([load(),load()]).map(lambda x: 2*x)
     # bag = db.from_delayed([load(),load()]).repartition(npartitions=4).map(lambda x: 2*x)
     out = bag.count().compute()
     print(out)
