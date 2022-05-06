b066d8b425f3b51c47bf0f1ac0d953edd68b421f
Yorai Levi <yorai.email@gmail.com>
2022-05-06 13:33:54 +0300

    May 6, 2022 1:33 PM

--- parallelism_test.py
+++ parallelism_test.py
@@ -7,7 +7,8 @@ def load():
     return delayed(range(N))
 if __name__ == '__main__':
     client = Client(LocalCluster())
-    bag = db.from_delayed([load(),load()]).repartition(npartitions=4).map(lambda x: 2*x)
+    bag = db.from_delayed([load(),load()]).map(lambda x: 2*x)
+    # bag = db.from_delayed([load(),load()]).repartition(npartitions=4).map(lambda x: 2*x)
     out = bag.count().compute()
     print(out)
 # df = bag.to_dataframe(meta=metaComment).body
