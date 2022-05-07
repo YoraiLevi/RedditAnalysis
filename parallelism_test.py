7774a909f2198a50ea59212daa10f2d7afed345b
Yorai Levi <yorai.email@gmail.com>
2022-05-07 04:44:07 +0300

    May 7, 2022 4:44 AM

--- parallelism_test.py
+++ parallelism_test.py
@@ -9,7 +9,7 @@ import ujson as json
 # def load():
 #     return delayed(range(N))
 if __name__ == '__main__':
-    client = Client(threads_per_worker=2, n_workers=2)
+    client = Client(threads_per_worker=2, n_workers=1)
     # client = Client()
 #     bag = db.from_delayed([load(),load()]).map(lambda x: 2*x)
 #     # bag = db.from_delayed([load(),load()]).repartition(npartitions=4).map(lambda x: 2*x)
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     def nothing(x):
         pass
     source = Stream()
-    source.buffer(10**8).scatter().map(json.loads).accumulate(lambda acc, x: acc + 1, start=0).gather().sink(nothing)
+    source.buffer(10**12).scatter().map(json.loads).accumulate(lambda acc, x: acc + 1, start=0).gather().sink(nothing)
     filename = "D:/Downloads/reddit/comments/RC_2021-05.zst"
     reader = Zreader(filename)
     for line in reader.readlines():
