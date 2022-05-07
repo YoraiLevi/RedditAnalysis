e3c636b162de2a86c14cd66cb6f0a2d33feb1e52
Yorai Levi <yorai.email@gmail.com>
2022-05-07 04:22:19 +0300

    May 7, 2022 4:22 AM

--- parallelism_test.py
+++ parallelism_test.py
@@ -1,32 +1,22 @@
 # import dask.bag as db
 from dask import delayed
 from dask.distributed import Client, LocalCluster
+from streamz import Stream
+from zreader import Zreader
 
 # N = 10**8
 # def load():
 #     return delayed(range(N))
-# if __name__ == '__main__':
-#     client = Client(LocalCluster())
+if __name__ == '__main__':
+    client = Client(LocalCluster())
 #     bag = db.from_delayed([load(),load()]).map(lambda x: 2*x)
 #     # bag = db.from_delayed([load(),load()]).repartition(npartitions=4).map(lambda x: 2*x)
 #     out = bag.count().compute()
 #     print(out)
 
-from dask.distributed import Client
-from streamz import Stream
-from time import sleep
-def inc(x):
-    sleep(1)  # simulate actual work
-    return x + 1
-if __name__ == '__main__':
-    client = Client()
-    # source = Stream()
-    # source.map(inc).sink(print)
-    # for i in range(10):
-        # source.emit(i)
     source = Stream()
-    source.scatter().map(inc).buffer(8).gather().sink(print)
-
+    source.scatter().map(inc).buffer(8)
+    filename = "D:/Downloads/reddit/comments/RC_2021-02.zst"
     for i in range(10):
         source.emit(i)
 
