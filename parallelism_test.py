4af803789907da84c55edf108d2add194709f237
Yorai Levi <yorai.email@gmail.com>
2022-05-07 04:15:21 +0300

    May 7, 2022 4:15 AM

--- parallelism_test.py
+++ parallelism_test.py
@@ -11,3 +11,21 @@
 #     # bag = db.from_delayed([load(),load()]).repartition(npartitions=4).map(lambda x: 2*x)
 #     out = bag.count().compute()
 #     print(out)
+
+from dask.distributed import Client
+from streamz import Stream
+from time import sleep
+def inc(x):
+    sleep(1)  # simulate actual work
+    return x + 1
+if __name__ == '__main__':
+    client = Client()
+    # source = Stream()
+    # source.map(inc).sink(print)
+    # for i in range(10):
+        # source.emit(i)
+    source = Stream()
+    source.scatter().map(inc).buffer(8).gather().sink(print)
+
+    for i in range(10):
+        source.emit(i)
