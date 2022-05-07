3b1ced1a27272566cec5d5b2aa69c6ee1717872f
Yorai Levi <yorai.email@gmail.com>
2022-05-07 04:12:25 +0300

    May 7, 2022 4:12 AM

--- parallelism_test.py
+++ parallelism_test.py
@@ -1,13 +1,13 @@
-import dask.bag as db
-from dask import delayed
-from dask.distributed import Client, LocalCluster
+# import dask.bag as db
+# from dask import delayed
+# from dask.distributed import Client, LocalCluster
 
-N = 10**8
-def load():
-    return delayed(range(N))
-if __name__ == '__main__':
-    client = Client(LocalCluster())
-    # bag = db.from_delayed([load(),load()]).map(lambda x: 2*x)
-    bag = db.from_delayed([load(),load()]).repartition(npartitions=4).map(lambda x: 2*x)
-    out = bag.count().compute()
-    print(out)
+# N = 10**8
+# def load():
+#     return delayed(range(N))
+# if __name__ == '__main__':
+#     client = Client(LocalCluster())
+#     bag = db.from_delayed([load(),load()]).map(lambda x: 2*x)
+#     # bag = db.from_delayed([load(),load()]).repartition(npartitions=4).map(lambda x: 2*x)
+#     out = bag.count().compute()
+#     print(out)
