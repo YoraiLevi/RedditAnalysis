52188237c6f43a6905f45ac63f388f55db152cab
Yorai Levi <yorai.email@gmail.com>
2022-05-09 00:00:10 +0300

    May 9, 2022 12:00 AM

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import dask.bag as db
 from dask.distributed import Client, progress
 
 if __name__ == '__main__':
-    client = Client(threads_per_worker=2, n_workers=2)
+    client = Client(threads_per_worker=2, n_workers=2,asynchronous=True)
     metaComment = [
         ("gilded", int),
         ("retrieved_on", int),
