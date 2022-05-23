c4b8b6d7e77ea02c9c81f43d05d89f3a5b0771f4
Yorai Levi <yorai.email@gmail.com>
2022-05-24 01:01:48 +0300

    May 24, 2022 1:01 AM

--- json_keys.py
+++ json_keys.py
@@ -11,10 +11,14 @@ args = parser.parse_args()
 
 glob_string = os.path.join(args.dir,'*.zst')
 files = glob.glob(glob_string)
+set_of_keys = set()
 for file in files:
     try:
         for string in islice(Zreader(file).readlines(),10):
-            print(string)
+            obj = json.loads(string)
+            keys = obj.keys()
+            set_of_keys.update(keys)
     except:
         pass
-    # print(file)
+for key in set_of_keys:
+    print(key)
