9981aa44d43f0ca0db6d7834ac34df255fe9725a
Yorai Levi <yorai.email@gmail.com>
2022-05-24 01:13:04 +0300

    May 24, 2022 1:13 AM

--- json_keys.py
+++ json_keys.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from itertools import islice
 import argparse
 import os
@@ -11,14 +12,16 @@ args = parser.parse_args()
 
 glob_string = os.path.join(args.dir,'*.zst')
 files = glob.glob(glob_string)
-set_of_keys = set()
+set_of_keys = defaultdict(set)
 for file in files:
     try:
         for string in islice(Zreader(file).readlines(),10):
             obj = json.loads(string)
-            keys = obj.keys()
-            set_of_keys.update(keys)
+            for key,item in obj.items():
+                if(isinstance(item,str) and len(item)>15):
+                    item = "this is likely a string"
+                set_of_keys[key].add(item)
     except:
         pass
-for key in set_of_keys:
-    print(key)
+for key,value in set_of_keys.items():
+    print(key,value)
