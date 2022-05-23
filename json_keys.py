3ce1b87ddfc570cb2d30484a07bc722a2f6c21d1
Yorai Levi <yorai.email@gmail.com>
2022-05-24 02:15:47 +0300

    May 24, 2022 2:15 AM

--- json_keys.py
+++ json_keys.py
@@ -1,11 +1,11 @@
 #  python .\json_keys.py E:\Datasets\reddit\comments > comment_field_types.txt
 #  python .\json_keys.py E:\Datasets\reddit\submissions\ > submission_field_types.txt
-from collections import defaultdict
+from collections import defaultdict, Counter
 from itertools import islice
 import argparse
 import os
 import glob
-from types import NoneType
+
 from zreader import Zreader
 import ujson as json
 
@@ -15,22 +15,29 @@ args = parser.parse_args()
 
 glob_string = os.path.join(args.dir,'*.zst')
 files = glob.glob(glob_string)
+files_keys = dict()
 for file in files:
-    set_of_keys = defaultdict(set)
+    keys = []
     try:
-        for string in islice(Zreader(file).readlines(),10000):
+        for string in islice(Zreader(file).readlines(),10):
             obj = json.loads(string)
-            for key,item in obj.items():
+            keys.append(list(map(obj.items(),lambda key,val: (key,type(val)))))
+
+            # for key,item in obj.items():
                 # if key == 'author_flair_richtext':
                     # print(file,string,item)
                 # if(isinstance(item,str) and len(item)>5):
                     # item = "this is likely a string"
-                set_of_keys[key].add(type(item))
+                # set_of_keys[key].add(type(item))
                 # if(isinstance(item,(list))):
                     # print(key,string)
-
     except:
         pass
-for key,value in set_of_keys.items():
+    print(keys)
+    file_keys = Counter(keys)
+    files_keys[file] = file_keys
+
+# for key,value in set_of_keys.items():
     # print(key,set(map(type,value)))
-    print(key,value)
+    # print(key,value)
+print(files_keys)
