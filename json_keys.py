7c18bf111e9e8af9c058a5ddd4f03adab002e63d
Yorai Levi <yorai.email@gmail.com>
2022-05-24 05:58:06 +0300

    May 24, 2022 5:58 AM

--- json_keys.py
+++ json_keys.py
@@ -4,8 +4,10 @@ from collections import defaultdict, Counter
 import collections
 from itertools import islice
 import argparse
+import itertools
 import os
 import glob
+from typing import Collection
 
 import pandas as pd
 
@@ -24,7 +26,7 @@ for file in files:
     keys = []
     types = []
     try:
-        for string in islice(Zreader(file).readlines(),10000):
+        for string in islice(Zreader(file).readlines(),10):
             obj = json.loads(string)
             for key,item in obj.items():
                 types.append((key,type(item)))
@@ -46,43 +48,5 @@ df['In Effect'] = LastUsage - FirstUsage + 1
 df['Since'] = FirstUsage
 df['Last'] = LastUsage
 df['Deprecated?'] = LastUsage < LastUsage.max()
-# def first_file(t):
-#     return min(filter(lambda filectr: t in filectr[1],files_keys.items()))[0]
-# def last_file(t):
-#     return max(filter(lambda filectr: t in filectr[1],files_keys.items()))[0]
-# def occurences_file(t):
-#     return len(list(filter(lambda filectr: t in filectr[1],files_keys.items())))
 
-# files_paths = sorted(files_keys.keys())
-# def print_stats(total):
-#     items = total.most_common(1)[0][1]
-#     for t,count in total.most_common():
-#         first_file_path = first_file(t)
-#         last_file_path = last_file(t)
-#         occurences_in_files = occurences_file(t)
-#         effetive_time = files_paths.index(last_file_path)-files_paths.index(first_file_path)
-#         print(effetive_time,occurences_in_files,first_file_path,last_file_path,t,":",count/items)
-
-# showupallset = set()
-
-# for file,total in files_keys.items():
-#     items = total.most_common(1)[0][1]
-#     most_common = total.most_common()
-#     showupall = filter(lambda item: item[1] == items, most_common)
-#     for item,count in showupall:
-#         showupallset.add(item)
-#     print(file,":")
-#     print_stats(total)
-
-# total_all_files = sum(files_keys.values(),collections.Counter())
-# print('all :')
-# print_stats(total_all_files)
-# # all_items = total_all_files.most_common(1)[0][1]
-# # print('most important :')
-# # important = [(i,total_all_files[i]/all_items) for i in showupallset]
-
-# # for (t,count) in sorted(important,key=lambda x: x[1],reverse=True):
-# #     first_file_path = first_file(t)
-# #     last_file_path = last_file(t)
-# #     time = files_paths.index(last_file_path)-files_paths.index(first_file_path)
-# #     print(time,last_file_path,first_file_path,t,count)
+itertools.groupby((t for (t,count) in sum(type_spreads.values(),Collection.Counter)),lambda x: x[0])
