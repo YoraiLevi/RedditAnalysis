67226389265e2caadd6783143208368046d1bf14
Yorai Levi <yorai.email@gmail.com>
2022-05-28 07:14:00 +0300

    May 28, 2022 7:14 AM

---
+++ p3.py
@@ -0,0 +1,17 @@
+import ujson
+
+base_data = {key:None for key in fields}
+def process_line(line):
+    obj = ujson.decode(line)
+    data = base_data.copy()
+    # data = defaultdict(None)
+    data["retrieved_utc"] = obj.get("retrieved_on")
+    data.update({key: obj[key] for key in obj if key in fields})
+    if isinstance(data["created_utc"], str):
+        data["created_utc"] = int(data["created_utc"])
+
+    left_over = {key: obj[key] for key in obj if key not in fields}
+
+    data["json"] = ujson.encode(left_over)
+    # print(data["json"])
+    return dict(data)
