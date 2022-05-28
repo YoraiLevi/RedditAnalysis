0f430542af7ed50e9c739404a376cacf9f070bc0
Yorai Levi <yorai.email@gmail.com>
2022-05-28 08:13:14 +0300

    May 28, 2022 8:13 AM

--- p3.py
+++ p3.py
@@ -1,6 +1,6 @@
 import ujson
-import sys
-sys.stdout.reconfigure(encoding='utf-8')
+# import sys
+# sys.stdout.reconfigure(encoding='utf-8')
 fields = [  # comment
     "author",
     "author_flair_text",
