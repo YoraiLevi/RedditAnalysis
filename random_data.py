2411ec8b5582fb5706f66c56ad4737e06a7c5e66
Yorai Levi <yorai.email@gmail.com>
2022-05-30 02:16:57 +0300

    May 30, 2022 2:16 AM

--- random_data.py
+++ random_data.py
@@ -134,7 +134,8 @@ if __name__ == "__main__":
     db = init_database()
     models = init_models(db,True)
     manager = Manager()
-    task_queue = manager.Queue()
+    task_queue1 = manager.Queue()
+    task_queue2 = manager.Queue()
     done_queue = manager.Queue()
     with db:
         db.create_tables([models["comment"], models["comment"]])
@@ -142,15 +143,21 @@ if __name__ == "__main__":
     ts = []
     try:
         for i in range(args.processes):
-            p = Process(target=worker, args=(task_queue, done_queue,args.threads,args.atomic,args.chunk_size_db))
+            p = Process(target=worker, args=(task_queue1, done_queue,args.threads,args.atomic,args.chunk_size_db))
             ps.append(p)
+        for i in range(args.processes):
+            p = Process(target=worker, args=(task_queue2, done_queue,args.threads,args.atomic,args.chunk_size_db))
+            ps.append(p)
+
         t = Thread(target=print_errors, args=[done_queue])
         ts.append(t)
         with open(args.file) as f:
             for line in islice(f.readlines(),0,1):
                 obj = process_line(line)
         for i in range(args.data_generators):
-            p = Process(target=generate_data, args=(int(args.n_rows/args.data_generators),args.chunk_size_enqueue, task_queue, obj))
+            p = Process(target=generate_data, args=(int(args.n_rows/args.data_generators/2),args.chunk_size_enqueue, task_queue1, obj))
+            ps.append(p)
+            p = Process(target=generate_data, args=(int(args.n_rows/args.data_generators/2),args.chunk_size_enqueue, task_queue2, obj))
             ps.append(p)
         for t in ts:
             t.start()
@@ -160,7 +167,9 @@ if __name__ == "__main__":
         print(traceback.format_exc())
     finally:
         for i in range(args.processes):
-            task_queue.put("STOP")
+            task_queue1.put("STOP")
+            task_queue2.put("STOP")
+
         running = False
         for p in ps:
             p.join()
